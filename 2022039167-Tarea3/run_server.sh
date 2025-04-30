0491ba0b66d40123a36dd63d429743444f20d065
PaoloInduni <paolo.induniocampo@gmail.com>
2025-04-29 18:47:34 -0600

    Updated documentation files

--- 2022039167-Tarea3/run_server.sh
+++ 2022039167-Tarea3/run_server.sh
@@ -5,4 +5,4 @@ gcc -o bin/prethread-WebServer src/prethread_webserver.c -lpthread
 gcc -o bin/HTTPclient src/http_client.c -lcurl
 
 # Ejecutar el servidor
-./bin/prethread-WebServer -n 5 -w /var/www/html -p 8080
+./bin/prethread-WebServer -n 5 -w ./data -p 8080
